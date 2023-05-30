71e0080e81e514a5fe5dd32fcefba2595958a2aa
kien <kien@kien>
2023-05-30 23:35:35 +0700

    swagger configuration
    https://betterprogramming.pub/documenting-ruby-on-rails-apis-using-rswag-gem-48c92e11ea30

--- newApi/config/routes.rb
+++ newApi/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  resources :cars
   resources :trails
   resources :forests
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
